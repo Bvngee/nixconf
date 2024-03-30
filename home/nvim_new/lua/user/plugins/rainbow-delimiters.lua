e5d8ec82cd0759afb64dfe2ae130cd2e3452a813
Jack N <nystromjp@gmail.com>
2024-03-30 12:49:52 -0700

    nvim v3: vim-sleuth

--- home/nvim_new/lua/user/plugins/rainbow-delimiters.lua
+++ home/nvim_new/lua/user/plugins/rainbow-delimiters.lua
@@ -1,27 +1,28 @@
 return {
-  -- 'hiphish/rainbow-delimiters.nvim',
-  -- event = 'BufEnter',
-  -- config = function()
-  --   local rd = require('rainbow-delimiters')
-  --   require('rainbow-delimiters.setup').setup({
-  --     -- all coppied from the readme
-  --     strategy = {
-  --       [''] = rd.strategy['global'],
-  --       vim = rd.strategy['local'],
-  --     },
-  --     query = {
-  --       [''] = 'rainbow-delimiters',
-  --       lua = 'rainbow-blocks',
-  --     },
-  --     highlight = {
-  --       'RainbowDelimiterRed',
-  --       'RainbowDelimiterOrange',
-  --       'RainbowDelimiterYellow',
-  --       'RainbowDelimiterGreen',
-  --       'RainbowDelimiterCyan',
-  --       'RainbowDelimiterBlue',
-  --       'RainbowDelimiterViolet',
-  --     },
-  --   })
-  -- end,
+  'hiphish/rainbow-delimiters.nvim',
+  event = 'BufEnter',
+  config = function()
+    local rd = require('rainbow-delimiters')
+    require('rainbow-delimiters.setup').setup({
+      -- all coppied from the readme
+      strategy = {
+        [''] = rd.strategy['global'],
+        -- vim = rd.strategy['local'],
+        zig = rd.strategy['noop'],
+      },
+      query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+      },
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterCyan',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterViolet',
+      },
+    })
+  end,
 }
