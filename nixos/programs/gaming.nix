04d3b4f6865ec0baf9d249ef3838e328ac20c586
Jack N <nystromjp@gmail.com>
2024-05-03 07:10:14 -0700

    todo

--- nixos/programs/gaming.nix
+++ nixos/programs/gaming.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  nix.settings = {
+  nix.settings = { # TODO: REMOVE
     # add github:fufexan/nix-gaming cachix
     substituters = [ "https://nix-gaming.cachix.org/" ];
     trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
