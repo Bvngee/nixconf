464724ea8aee9232d096700d63b1251001378a5e
Jack N <nystromjp@gmail.com>
2024-08-20 14:37:06 -0700

    add link

--- nixos/nix-ld.nix
+++ nixos/nix-ld.nix
@@ -2,8 +2,7 @@
   programs.nix-ld = {
     enable = true;
     package = pkgs.nix-ld-rs;
-    libraries = [
-
-    ];
+    # See defaults here: https://github.com/NixOS/nixpkgs/blob/5bb0c5ac60d5dc25c08ce3910ff75ea1da0e4026/nixos/modules/programs/nix-ld.nix#L44-L59
+    libraries = [ ];
   };
 }
