15b4f804e25d7de422a066b1253f8c011cb58f7f
Jack N <nystromjp@gmail.com>
2024-08-31 04:07:43 -0700

    fix polkit agent lmao

--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -7,24 +7,26 @@
     ./swww.nix
   ];
 
-  home.packages =
-    with pkgs; [
-      grim
-      slurp
-      wayshot
-      wl-clipboard
-      wlr-randr
-      wf-recorder
-      wev
-      libnotify
-      playerctl
-      satty
-      inputs.woomer.packages.${system}.default
-      hyprpicker
-      wlsunset
+  home.packages = with pkgs; [
+    grim
+    slurp
+    wayshot
+    wl-clipboard
+    wlr-randr
+    wf-recorder
+    wev
+    libnotify
+    playerctl
+    satty
+    inputs.woomer.packages.${system}.default
+    hyprpicker
+    wlsunset
 
-      # Preferred PolKit agent
-      pkgs.pantheon.pantheon-agent-polkit
-      # pkgs.libsForQt5.polkit-kde-agent
-    ];
+    # Preferred PolKit agent
+    # (note that since these binaries are in /libexec they are
+    # only accessible from /run/current-system/sw/libexec/..)
+    mate.mate-polkit
+    # pantheon.pantheon-agent-polkit
+    # libsForQt5.polkit-kde-agent
+  ];
 }
