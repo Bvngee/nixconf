1dee9c9c95118140b87ff09ebeafd56057781b19
Jack N <nystromjp@gmail.com>
2024-09-07 00:47:02 -0700

    make networkmanager isMobile only

--- nixos/hardware/network.nix
+++ nixos/hardware/network.nix
@@ -1,11 +1,12 @@
-{ config, ... }: {
+{ lib, config, ... }: {
 
   networking.hostName = config.profile.hostname;
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
   # Do I want isMobile only here?
-  networking.networkmanager.enable = true;
-  users.users.${config.profile.mainUser}.extraGroups = [ "networkmanager" ];
+  networking.networkmanager.enable = config.profile.isMobile;
+  users.users.${config.profile.mainUser}.extraGroups =
+    lib.optional (config.profile.isMobile) "networkmanager";
 
   # spams annoying notifications, tray menu is barely useful
   # programs.nm-applet = {
