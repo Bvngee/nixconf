e9e71cd5a30a49cd3001193b520fe96fbe07ccd4
Jack N <nystromjp@gmail.com>
2024-10-20 01:29:26 -0700

    add .... aliases

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -37,6 +37,13 @@
     cd = "z";
     cdi = "zi";
 
+    # a little overkill but hey why not
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    "....." = "cd ../../../..";
+    "......" = "cd ../../../../..";
+
     # best fetch so far
     fetch = "fastfetch --structure Title:Separator:OS:Host:Kernel:Uptime:Packages:Shell:Display:DE:WM:WMTheme:Theme:Icons:Font:Cursor:Terminal:TerminalFont:CPU:GPU:Break:Colors";
 
