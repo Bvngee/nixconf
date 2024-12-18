e58a6d722e72756e305f78358aa860ff1e029b59
Jack N <nystromjp@gmail.com>
2024-12-18 02:54:53 -0800

    fix "Neovim wrapper" to user $TERMINAL

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -20,4 +20,22 @@
       fd
     ];
   };
+
+  # Replace the default nvim.desktop (found at
+  # ~/.nix-profile/share/applications/nvim.desktop for HM) with a copy that sets
+  # Terminal=False and launches neovim with $TERMINAL instead of letting whatever
+  # the launching application is choose the terminal emulator
+  home.file.".local/share/applications/nvim.desktop".source =
+    let
+      nvim = config.programs.neovim.package;
+    in
+    "${pkgs.runCommand "fix-nvim-desktop" {} ''
+      mkdir -p $out
+      cp ${"${nvim}/share/applications/nvim.desktop"} $out/nvim.desktop
+      substituteInPlace $out/nvim.desktop \
+        --replace-fail "Exec=nvim %F" "Exec=sh -c \"\$TERMINAL nvim %F\"" \
+        --replace-fail "Terminal=true" "Terminal=false"
+    ''}/nvim.desktop";
 }
+
+
