8f7448c4569627dad177a49a2bf0867d2def8609
Jack N <nystromjp@gmail.com>
2025-02-17 23:30:43 -0800

    make libinput fast clicking config unconditional

--- nixos/hardware/libinput.nix
+++ nixos/hardware/libinput.nix
@@ -1,10 +1,9 @@
-{ config, lib, ... }: {
+{ ... }: {
   # Not sure if I need this? It's already enabled anyways so shouldn't make a difference ¯\_(ツ)_/¯
   services.libinput.enable = true;
 
-  # For stationary devices (on which I will likely do some gaming), allow
-  # mice to click fast. TODO: make the condition more sensible than !isMobile
-  environment.etc."libinput/local-overrides.quirks" = lib.mkIf (!config.profile.isMobile) {
+  # Allow mice to click fast
+  environment.etc."libinput/local-overrides.quirks" = {
     text = ''
       [Disable Mouse Debounce Protection]
       MatchUdevType=mouse
