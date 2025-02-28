99f4260585b71347a2f3c2154b61c78de6303479
Jack N <nystromjp@gmail.com>
2025-02-28 02:35:33 -0800

    FIXED CHROMIUM TAKING MINUTES TO LAUNCH LETS GO

--- profiles/precision/nixos/default.nix
+++ profiles/precision/nixos/default.nix
@@ -1,5 +1,5 @@
 # Custom configuration for Dell Precision laptop
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./nvidia.nix
@@ -7,6 +7,14 @@
 
   # Stolen from nixos-hardware
   boot.kernelParams = [ "i915.enable_fbc=1" "i915.enable_psr=2" "i915.modeset=1" ];
+
+  # Force i915 driver to load BEFORE anything nvidia-related. Fixes a race
+  # condition that causes anything chromium-based to take multiple minutes
+  # to start and to spam literal gigabytes of journal logs. See:
+  # https://forums.developer.nvidia.com/t/550-54-14-cannot-create-sg-table-for-nvkmskapimemory-spammed-when-launching-chrome-on-wayland/284775/37
+  # https://www.reddit.com/r/archlinux/comments/1cn10w4/nvidia_drm_cannot_create_sg_table_message_spam_in/
+  # https://issues.chromium.org/issues/351095641
+  boot.kernelModules = lib.mkOrder 100 [ "i915" ];
 
   hardware.opengl.extraPackages = [ pkgs.intel-media-driver ];
   hardware.opengl.extraPackages32 = [ pkgs.pkgsi686Linux.intel-media-driver ];
