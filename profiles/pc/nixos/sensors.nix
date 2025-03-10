287d99874f3d8b46dfd1fb051f0e508bf125ce51
Jack N <nystromjp@gmail.com>
2025-03-10 00:08:28 -0700

    use coolercontrol from pkgsUnstable

--- profiles/pc/nixos/sensors.nix
+++ profiles/pc/nixos/sensors.nix
@@ -1,5 +1,5 @@
-{ config, lib, ... }: {
-  
+{ pkgsUnstable, config, lib, ... }: {
+
   # On 3/9/25, I ran `sudo sensors-detect` and it told me to add these kernel
   # modules. After adding them here, the `sensors` command correctly reports the
   # rest of the fan RPMs and temps that weren't reported previously.
@@ -13,7 +13,30 @@
   # case, aio, gpu).
   # TODO(24.11): newer version of this should fix setting gpu fan curve (no
   # longer uses nvidia-settings!)
-  programs.coolercontrol.enable = true;
+
+  # ----- TODO(24.11): replace with enable option in 24.11 -----
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.11/nixos/modules/programs/coolercontrol.nix#L28 
+  systemd = {
+    packages = with pkgsUnstable.coolercontrol; [
+      coolercontrol-liqctld
+      coolercontrold
+    ];
+    services = {
+      coolercontrol-liqctld.wantedBy = [ "multi-user.target" ];
+      coolercontrold.wantedBy = [ "multi-user.target" ];
+      coolercontrold.path =
+        let
+          nvidiaPkg = config.hardware.nvidia.package;
+        in
+        [
+          nvidiaPkg # nvidia-smi
+          nvidiaPkg.settings # nvidia-settings
+        ];
+    };
+  };
+  # ----- end todo -------------------------------------------
+
+  #programs.coolercontrol.enable = false;
   # note: programs.coolercontrol.nvidiaSupport is set automatically
 
 }
