17b787748225d6d46cc97fc3eb6d363d5f014ed2
Jack N <nystromjp@gmail.com>
2025-03-22 16:44:45 -0700

    open tailscale port in firewall

--- nixos/programs/tailscale.nix
+++ nixos/programs/tailscale.nix
@@ -2,5 +2,10 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
+    # should make it easier for peer-to-peer connections to be established in
+    # tricky nested-NAT and tough firewall scenarios 
+    # https://tailscale.com/blog/how-nat-traversal-works
+    # https://tailscale.com/kb/1082/firewall-ports
+    openFirewall = true;
   };
 }
